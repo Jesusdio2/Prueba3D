f3745dd6eb4325e76993f32774eb8de266285feb
Jesusdio2 <frankwither991000@gmail.com>
2025-10-04 21:48:52 -0700

    Create SaveController.cs

---
+++ Assets/Scripts/SaveController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SaveController : MonoBehaviour // Not used for now.
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
