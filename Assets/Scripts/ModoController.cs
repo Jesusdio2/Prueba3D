fb6b6e0bba434d211476dbaa3e3ae4633d983476
Jesusdio2 <frankwither991000@gmail.com>
2025-10-04 21:46:53 -0700

    Create ModoController.cs

---
+++ Assets/Scripts/ModoController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ModoController : MonoBehaviour // Not Used For Now.
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public enum TipoDeModo
+    {
+        Supervivencia,
+        Creativo
+    }
+
+}
